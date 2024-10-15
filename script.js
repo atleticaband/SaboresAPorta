57e93339cb6ee99cafd409d754c6a38303f9753b
atleticaband <canvapro7130@gmail.com>
2024-10-15 11:36:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Adicione funcionalidades com JavaScript aqui, como:
+// - Interação com o menu de navegação
+// - Mudança de conteúdo dinâmica
+// - Efeito de animação
+// - Tratamento de formulários
+// - Busca de restaurantes
+// - Integração com API para dados dos restaurantes
